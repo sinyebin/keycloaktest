08ccfb464927bbf7b809c4c9b74170412fec8bba
sinyebin <dpqls0625@naver.com>
2023-11-06 00:55:32 +0900

    커밋

--- src/main/java/com/example/keycloaktest/SecurityConfiguration.java
+++ src/main/java/com/example/keycloaktest/SecurityConfiguration.java
@@ -2,11 +2,16 @@ package com.example.keycloaktest;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,6 +20,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Value("${user.redirect}")
+    private String redirect;
+
+    @Value("${keycloak.auth-server-url}")
+    private String authServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,13 +39,40 @@ public class SecurityConfiguration {
                         .requestMatchers("/user/**").hasRole("USER") // USER 역할을 가진 사용자만 접근 가능
                         .anyRequest().authenticated() // 나머지 요청은 모두 인증이 필요함
                 )
-                .oauth2Login(withDefaults()) // OAuth2 로그인 활성화
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .defaultSuccessUrl("/", true)) // OAuth2 로그인 활성화
                 // OAuth2 리소스 서버 설정 (JWT 토큰 사용)
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
-                )
+                )// 로그아웃 설정 추가
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            String idTokenHint = null;
 
-                // CSRF 설정 비활성화 (REST API의 경우 필요할 수 있음)
+                            if (authentication instanceof OAuth2AuthenticationToken) {
+                                OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+                                OidcUser oidcUser = (OidcUser) oauthToken.getPrincipal();
+                                idTokenHint = oidcUser.getIdToken().getTokenValue();
+                            }
+
+                            if (idTokenHint != null) {
+                                String logoutUrl = authServerUrl + "realms/" + realm +
+                                        "/protocol/openid-connect/logout?post_logout_redirect_uri=" +
+                                        URLEncoder.encode(redirect, StandardCharsets.UTF_8.toString()) +
+                                        "&id_token_hint=" + idTokenHint;
+
+                                response.sendRedirect(logoutUrl);
+                            } else {
+                                // ID 토큰이 없는 경우의 처리
+                                String logoutUrl = authServerUrl + "realms/" + realm +
+                                        "/protocol/openid-connect/logout?redirect_uri=";
+                                response.sendRedirect(logoutUrl);
+                            }
+
+                        })
+                )
+        // CSRF 설정 비활성화 (REST API의 경우 필요할 수 있음)
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
--- src/main/java/com/example/keycloaktest/TestController.java
+++ src/main/java/com/example/keycloaktest/TestController.java
@@ -1,35 +1,55 @@
 package com.example.keycloaktest;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.Principal;
 import java.util.HashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@Controller
 public class TestController {
+
+
+
 
     @Autowired
     KeycloakUserService keycloakUserService;
 
-    @GetMapping(path = "/")
-    public HashMap index() {
-        // get a successful user login
-        OAuth2User user = ((OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        return new HashMap(){{
-            put("hello", user.getAttribute("name"));
-            put("your email is", user.getAttribute("email"));
-        }};
+    @GetMapping("/")
+    public String index(Model model) {
+        OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2User user = authentication.getPrincipal();
+        String loginType = authentication.getAuthorizedClientRegistrationId(); // "google" 또는 다른 클라이언트
+
+        model.addAttribute("name", user.getAttribute("name"));
+        model.addAttribute("email", user.getAttribute("email"));
+        model.addAttribute("loginType", loginType);
+
+        return "index";
     }
 
 
+    @ResponseBody
     @GetMapping(path = "/unauthenticated")
     public HashMap unauthenticatedRequests() {
         return new HashMap(){{
@@ -37,12 +57,14 @@ public class TestController {
         }};
     }
 
+    @ResponseBody
     @GetMapping("/test")
     public Principal getUser(Principal principal) {
         // 사용자 정보 반환
         return principal;
     }
 
+    @ResponseBody
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest) {
         System.out.println("1111"+signUpRequest.getUsername()+" "+ signUpRequest.getPassword());
@@ -50,4 +72,23 @@ public class TestController {
         return ResponseEntity.ok().build();
     }
 
+//    @GetMapping("/custom_logout")
+//    public String logout(HttpServletRequest request, HttpServletResponse response) {
+//        System.out.println(111111);
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//
+//        if (auth != null) {
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//        }
+//        // Keycloak 로그아웃 엔드포인트로 리다이렉트
+//        return "redirect:" + authServerUrl + "/realms/" + realm +
+//                "/protocol/openid-connect/logout?redirect_uri=" + URLEncoder.encode(redirect, StandardCharsets.UTF_8);
+//    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        // 실제 로그아웃 처리는 SecurityConfiguration에서 설정했습니다.
+        // 이 메소드는 단지 엔드포인트를 제공하기 위함입니다.
+        return "redirect:/";
+    }
 }
