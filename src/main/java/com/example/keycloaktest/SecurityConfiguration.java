de724b98d8fceccfe00c50672761df305cb7e621
sinyebin <dpqls0625@naver.com>
2023-11-05 20:18:44 +0900

    커밋

--- src/main/java/com/example/keycloaktest/SecurityConfiguration.java
+++ src/main/java/com/example/keycloaktest/SecurityConfiguration.java
@@ -36,7 +36,6 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
